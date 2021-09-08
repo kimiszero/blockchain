1bd9a0b29e23a4b0368336210263c2dd51fb9cd3
김은영 <77275707+kimiszero@users.noreply.github.com>
2021-09-08 13:28:54 +0900

    0908

--- solidity/operation.sol
+++ solidity/operation.sol
@@ -3,9 +3,6 @@ pragma solidity ^0.4.11;
 contract Example005 {
   function operation() returns (uint result) {
     result = sum(11,5);
-    result = minus(11,5);
-    result = muliply(11,5);
-    result = devide(11,5);
   }
 
   function sum(uint a, uint b) returns (uint) {
