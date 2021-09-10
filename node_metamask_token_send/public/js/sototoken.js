96f4d8e7806e7287fe1392b8c9f674b137746202
김은영 <77275707+kimiszero@users.noreply.github.com>
2021-09-10 16:05:30 +0900

    0910

---
+++ node_metamask_token_send/public/js/sototoken.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    const ContractAddress = '0xF3A1C565f0038243804fb9D295D2f1d06dd6c31A';
+    // const ContractABI = $.getScript("/js/contractabi.json");
+
+    $('#linkHome').click(function () { showView("viewHome") });
+    $('#linkSubmitItem').click(function () { showView("viewSubmitItem"); });
+
+    $('#account').val(ContractAddress);
+    $('#contractLink').text(ContractAddress);
+    $('#contractLink').attr('href', 'https://ropsten.etherscan.io/address/' + ContractAddress);
+
+    // Attach AJAX "loading" event listener
+    $(document).on({
+        ajaxStart: function () { $("#loadingBox").show() },
+        ajaxStop: function () { $("#loadingBox").hide() }
+    });
+
+    function showView(viewName) {
+        // Hide all views and show the selected view only
+        $('main > section').hide();
+        $('#' + viewName).show();
+    }
+
+    function showInfo(message) {
+        $('#infoBox>p').html(message);
+        $('#infoBox').show();
+        $('#infoBox>header').click(function () { $('#infoBox').hide(); });
+    }
+
+    function showError(errorMsg) {
+        $('#errorBox>p').html("Error: " + errorMsg);
+        $('#errorBox').show();
+        $('#errorBox>header').click(function () { $('#errorBox').hide(); });
+    }
+
+});
+
